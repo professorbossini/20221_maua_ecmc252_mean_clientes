30ece6f103bd0a4f7f602506361e88a940b4a985
Rodrigo Bossini <professorbossini@gmail.com>
2022-05-14 12:18:04 -0300

    apostila02-mean: fim da secao 2.1
    substituicao de two way data binding por template driven form

--- src/app/clientes/cliente-inserir/cliente-inserir.component.ts
+++ src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core'
+import { NgForm } from '@angular/forms';
 import { Cliente } from '../cliente.model';
 
 @Component({
@@ -15,11 +16,12 @@ export class ClienteInserirComponent{
     fone: string
     email: string
 
-    onAdicionarCliente(){
+    onAdicionarCliente(form: NgForm){
+        if(form.invalid) return
         const cliente: Cliente = {
-            nome: this.nome,
-            fone: this.fone,
-            email: this.email
+            nome: form.value.nome,
+            fone: form.value.fone,
+            email: form.value.email
         }
         this.clienteAdicionado.emit(cliente)
     }
